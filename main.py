7bc8d7d230aa2cd1ef32e569d5d03abb22e1ae96
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 21:29:23 +0900

    [라우터] main.py에 authentication 라우터 등록

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from DB import models
 from DB.database import engine
 from Router import user 
 from Router import post
+from Auth import authentication
 
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
+app.include_router(authentication.router)
+
 
 @app.get("")
 def root():
