3a8059df6608cf438f977444fd91de2e3e2decaf
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 20:40:57 +0900

    [라우터] Post 라우터에 "/all" 엔드 포인트 추가

--- Router/post.py
+++ Router/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm.session import Session
 from Router.schemas import PostBase, PostDisplay
 from DB.database import get_db
 from DB import db_post
+from typing import List
 
 router = APIRouter(
     prefix="/post",
@@ -17,3 +18,7 @@ def create(request: PostBase, db: Session = Depends(get_db)):
     if not request.image_url_type in image_url_types:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail = "parameter image_url_type is wrong")
     return db_post.create(db,request)
+
+@router.get("/all", response_model = List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
