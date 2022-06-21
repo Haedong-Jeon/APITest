0e99500bee1d505244afdaba4777b1ae71430ebc
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 20:28:50 +0900

    [라우터] Post 라우터 추가

---
+++ Router/post.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from sqlalchemy.orm.session import Session
+from Router.schemas import PostBase, PostDisplay
+from DB.database import get_db
+from DB import db_post
+
+router = APIRouter(
+    prefix="/post",
+    tags = ["post"]
+)
+
+image_url_types = ["absolute", "relative"]
+
+@router.post("")
+def create(request: PostBase, db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail = "parameter image_url_type is wrong")
+    return db_post.create(db,request)
