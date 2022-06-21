4317b6641db832ba4632cb66df402ec55d11a037
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 22:28:23 +0900

    [라우터] Post에 "delete/{id}" 엔드 포인트 추가

--- DB/db_post.py
+++ DB/db_post.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from Router.schemas import PostBase
 from sqlalchemy.orm.session import Session
 from DB.models import DbPost
@@ -18,3 +19,15 @@ def create(db: Session, request: PostBase):
 
 def get_all(db:Session):
     return db.query(DbPost).all()
+
+def delete(db: Session, id: int, user_id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"post with id ${id} not found")
+    
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail = f"only post creator can delete post")
+    
+    db.delete(post)
+    db.commit()
+    return "OK"
