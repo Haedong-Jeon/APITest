6a836d166c99649e34fa77bb6dc55b7b9df6cd16
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 19:29:15 +0900

    [데이터베이스] 비밀번호 해싱 추가

--- DB/db_user.py
+++ DB/db_user.py
@@ -1,12 +1,13 @@
 from Router.schemas import UserBase
 from sqlalchemy.orm.session import Session
+from DB.hashing import Hash
 from DB.models import DbUser 
 
 def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username = request.username,
         email = request.email,
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
