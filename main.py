a821bc32e7587f5712cfc98b13a07ea5692b5dee
Haedong-Jeon <haedongjeon@icloud.com>
2022-06-21 19:08:34 +0900

    [데이터베이스] 데이터베이스 생성

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from DB import models
+from DB.database import engine
 
 app = FastAPI()
 
 @app.get("")
 def root():
     return "Hello world";
+
+models.Base.metadata.create_all(engine)
